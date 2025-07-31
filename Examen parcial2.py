ed06c9c9109bbefb9be106331a7823014860d945
escartigui <escarlethmayali@gmail.com>
2025-07-30 19:21:31 -0600

    scar

--- Examen parcial2.py
+++ Examen parcial2.py
@@ -1,1 +1,67 @@
-print("holis")
+class Menu():
+
+    def Mcd(self, a, b):
+        if b == 0:
+            return 0
+        else:
+            return self.Mcd(b % a, b)
+
+    def palabraencadena(self, r):
+        if r =="":
+            return 0
+        else:
+            if r[0] == "":
+                return 1
+            else:
+                self.palabraencadena(r[1:])
+        return 1 + self.palabraencadena(r[1:])
+
+    def calcudigi(self,n):
+        if n == 0:
+            return 0
+        else:
+            return n[-1] + self.calcudigi(n[-1])
+
+    def cadenarepetida(self,n):
+        if n == 0:
+            return 1
+        else:
+            return n * self.cadenarepetida(n)
+
+
+class menu():
+    def __init__(self):
+        self.prueba = Menu()
+
+    while True:
+     print("MENU")
+     print("1.Calcular Mcd")
+     print("2.Repetir una palabra")
+     print("3.repetir palabras")
+     print("4.convertir decimal a binario")
+     print("5.calcular cuantos digitos tiene un numero")
+     print("6.SALIR")
+
+     op = int(input("Ingresa una opcion: "))
+
+     if op == 1:
+      a = int(input("Ingresa un numero: "))
+      b = int(input("Ingresa otro numero: "))
+
+     if op == 2:
+      palabras = int(input("Ingresa una palabra: "))
+
+      if op == 3:
+          n = input("Ingrese un texto: ")
+
+      if op == 4:
+          print("wena noche")
+
+     if op == 5:
+         dato = int(input("Ingrese el numero para calcular sus digitos: "))
+
+
+     if op == 6:
+          print("Hasta que nos volvamos a ver")
+     break
+    menu = Menu
